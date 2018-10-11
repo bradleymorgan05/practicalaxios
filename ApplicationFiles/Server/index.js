e84f4dc211e317cb2d6195f3b45d64f69497167e
Justin Chung <bearlyalive1337@gmail.com>
2018-10-11 09:37:30 -0400

    (fix) added gitignore

--- ApplicationFiles/Server/index.js
+++ ApplicationFiles/Server/index.js
@@ -1,17 +1,6 @@
 const express = require("express");
-const mongoose = require("mongoose");
 // allow for references to our path
 const path = require("path");
-
-// connect to external database using mongoose
-mongoose.connect(
-  "mongodb://axios:axios123@ds015899.mlab.com:15899/practicalaxios",
-  { useCreateIndex: true, useNewUrlParser: true }
-);
-// Upon attempted connection, log result
-mongoose.connection
-  .once("open", () => console.log("Connected to MongoDB.."))
-  .on("error", error => console.log("Error connecting to DB"));
 
 // Create an app instance of express
 const app = express();
